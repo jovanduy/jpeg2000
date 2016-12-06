567e40ad9ee98811485feb73cfab6ada807690aa
Jordan Van Duyne <jordannn808@gmail.com>
2016-12-05 21:57:46 -0500

    basic template

---
+++ compress.py
@@ -0,0 +1,29 @@
+###################################
+# JPEG2000 Image Compression script
+###################################
+
+image = "data/image.jpg"
+
+def image_tiling(img):
+    # tile image
+
+def dc_level_shift(img):
+    # dc level shifting
+
+def component_transformation(img):
+    # component transformation:
+    # split image into Y, Cb, Cr
+
+def dwt(img):
+    # do the mathmagic dwt
+
+def quantization(img):
+    # quantization
+
+def entropy_coding(img):
+    # encode image
+
+def bit_stream_formation(img):
+    # idk if we need this or what it is
+
+
